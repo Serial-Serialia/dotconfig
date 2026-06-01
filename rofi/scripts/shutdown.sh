98562c9704cb21b0e15973f78e12a8e53a033ccf
Serial-Serialia <jgabrielsoares04@gmail.com>
2026-06-01 14:54:04 -0300

    Fixing rofi logout script

--- rofi/scripts/shutdown.sh
+++ rofi/scripts/shutdown.sh
@@ -3,7 +3,7 @@
 option=$(printf "Logout\nLock\nRestart\nShutdown" | rofi -dmenu -p "")
 
 case "$option" in
-	"Logout") -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit ;;
+	"Logout") -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch "hl.dsp.exit()" ;;
 	"Lock") hyprlock ;;
 	"Restart") systemctl reboot ;;
 	"Shutdown") systemctl poweroff ;;
